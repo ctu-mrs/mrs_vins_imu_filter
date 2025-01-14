145cde80c07186213df2a1b83629312c17cd83d9
Matej Petrlik <matej.petrlik@fel.cvut.cz>
2025-01-14 15:21:03 +0100

    added indent to butterworth filter design script output

--- scripts/filter_design.py
+++ scripts/filter_design.py
@@ -9,12 +9,13 @@ order = 1
 
 # Critical frequency of the filter [Hz]
 # For a Butterworth filter, this is the point at which the gain drops to 1/sqrt(2) that of the passband (the “-3 dB point”).
-threshold_freq = 10
+critical_freq = 10
 
 # Sampling frequency of the data [Hz] (find out using e.g. 'rostopic hz' on the IMU topic)
 sampling_freq = 200
 
-[b, a] = scipy.signal.butter(order, threshold_freq, btype='low', analog=False, output='ba', fs=sampling_freq)
+[b, a] = scipy.signal.butter(order, critical_freq, btype='low', analog=False, output='ba', fs=sampling_freq)
 
-print("a: [" + str(a[0]) + ", " + str(a[1]) + "]")
-print("b: [" + str(b[0]) + ", " + str(b[1]) + "]")
+print("Butterworth filter order: " + str(order) + ", critical frequency: " + str(critical_freq) + " Hz, sampling frequency: " + str(sampling_freq) + " Hz, parameters: ")
+print("    a: [" + str(a[0]) + ", " + str(a[1]) + "]")
+print("    b: [" + str(b[0]) + ", " + str(b[1]) + "]")
